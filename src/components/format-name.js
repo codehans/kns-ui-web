a4c316d721c812355ac25edea28a4930fdd58ec7
mintthemoon <mint@mintthemoon.xyz>
2022-11-30 17:15:01 -0500

    count graphemes for formatname shortening

--- src/components/format-name.js
+++ src/components/format-name.js
@@ -1,19 +1,23 @@
 import * as React from "react"
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
+import GraphemeSplitter from "grapheme-splitter"
+
+const splitter = new GraphemeSplitter()
 
 const FormatName = ({children, len = 16}) => {
   if (!children || typeof children != "string") {
     return <span></span>
   }
-  if (children.length <= len) {
+  const chars = splitter.splitGraphemes(children)
+  if (chars.length <= len) {
     return <span>{children}</span>
   } else {
     const display = [
-        children.slice(0, 6),
+        chars.slice(0, 6),
         "...",
-        children.slice(children.length - 6),
-    ].reduce((x, y) => x.concat(y))
+        chars.slice(chars.length - 6),
+    ].flat(1).reduce((x, y) => x.concat(y))
     return (
       <OverlayTrigger placement="bottom" overlay={<Tooltip>{children}</Tooltip>}>
         <span>{display}</span>
